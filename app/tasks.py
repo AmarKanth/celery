bd4a1a59e95d9a6d6dd97e40c5b922f412fa5ede
Amar Kanth BOLLU <amarbo.in@mouritech.com>
2023-04-13 17:44:52 +0530

    changes in tasks

--- app/tasks.py
+++ app/tasks.py
@@ -1,11 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
-from celery.task.schedules import crontab
-from celery.decorators import periodic_task
 
 
 @shared_task
-@periodic_task(run_every=crontab(minute=1))
 def add(x, y):
     return x + y
